43220442f0a5872236b3bfff76981e392d33770c
supperley <v.bobrovko@gmail.com>
2023-08-01 19:14:22 +0300

    add data fetch

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
 import Sort from './components/Sort';
 import './scss/app.scss';
-import pizzas from './assets/pizzas.json';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch('https://64c92e89b2980cec85c20458.mockapi.io/items')
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  });
+
   return (
     <div class="wrapper">
       <Header />
@@ -17,7 +24,7 @@ function App() {
           </div>
           <h2 class="content__title">Все пиццы</h2>
           <div class="content__items">
-            {pizzas.map((pizza) => (
+            {items.map((pizza) => (
               <PizzaBlock key={pizza.id} {...pizza} />
             ))}
           </div>
